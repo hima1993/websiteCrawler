b124c7129386ba03c63818f2f986140d7e09007d
hima1993 <himarukrox@gmail.com>
2022-06-16 09:32:23 +0530

    refactor the ProgressDisplay.java class

--- web-content-crawler/src/main/java/com/web/content/crawler/ProgressDisplay.java
+++ web-content-crawler/src/main/java/com/web/content/crawler/ProgressDisplay.java
@@ -9,117 +9,62 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 /**
- *
  * @author himaruksilva
  */
-public class ProgressDisplay 
-{
+public class ProgressDisplay extends Thread {
     private float currentProgress = 0.0f;
-    private float totalAmountToDownload=100.0f;
+    private float totalAmountToDownload = 100.0f;
     private float currentAmountToDownload = 0.0f;
-   
-    private static volatile ProgressDisplay progressDisplayInstance= null;
 
-    private ProgressDisplay() {};
-    
-    public static ProgressDisplay getInstance()
-    {
-        if(progressDisplayInstance == null)
-        {    
-            synchronized (ContentCrawlController.class)
-            {
-                if(progressDisplayInstance == null)
-                {
-                    progressDisplayInstance = new ProgressDisplay();
-                }
-            }
-        }
-        return progressDisplayInstance;
-    }
 
-    /**
-     * @return the currentProgress
-     */
-    public float getCurrentProgress() 
+    @Override
+    public void run() 
     {
-        return currentProgress;
-    }
-
-    /**
-     * @param currentProgress the currentProgress to set
-     */
-    public void setCurrentProgress(float currentProgress) 
-    {
-        this.currentProgress = currentProgress;
-    }
-
-    /**
-     * @return the totalAmountToDownload
-     */
-    public float getTotalAmountToDownload() 
-    {
-        return totalAmountToDownload;
+        show();
     }
 
     /**
      * @param totalAmountToDownload the totalAmountToDownload to set
      */
-    public void setTotalAmountToDownload(float totalAmountToDownload) 
-    {
+    public void setTotalAmountToDownload(float totalAmountToDownload) {
         this.totalAmountToDownload = totalAmountToDownload;
     }
 
-    /**
-     * @return the currentAmountToDownload
-     */
-    public float getCurrentAmountToDownload() 
-    {
-        return currentAmountToDownload;
-    }
 
     /**
      * @param currentAmountToDownload the currentAmountToDownload to set
      */
-    public void setCurrentAmountToDownload(float currentAmountToDownload) 
-    {
+    public void setCurrentAmountToDownload(float currentAmountToDownload) {
         this.currentAmountToDownload = currentAmountToDownload;
     }
 
-    
-    public void show()
+
+    public void show() 
     {
         try 
         {
-            while(true)
+            while (this.currentProgress != 100.0) 
             {
-                if(this.currentProgress != 100.0)
+                this.currentProgress = (int) ((this.currentAmountToDownload * 100) / this.totalAmountToDownload);
+
+                StringBuilder bar = new StringBuilder("[");
+
+                for (int i = 10; i <= this.currentProgress; i += 10) 
                 {
-                    this.currentProgress = (int)((this.currentAmountToDownload * 100)/this.totalAmountToDownload);
-
-                    String bar = "[";
-                    
-                    for(int i=10;i<=this.currentProgress;i+=10)
-                    {
-                        bar += "*";
-                    }
-
-                    bar+= "]";
-
-                    System.out.print("Current Progress: "+bar+"["+this.currentProgress +"%]\r");
-
-                    Thread.sleep(100);
+                    bar.append("*");
                 }
-                else
-                {   
-                    break;
-                }
+
+                bar.append("]");
+
+                System.out.print("Current Progress: " + bar + "[" + this.currentProgress + "%]\r");
+
+                Thread.sleep(100);
             }
         } 
-        catch (Exception ex) 
+        catch (Exception ex)
         {
             Logger.getLogger(ProgressDisplay.class.getName()).log(Level.SEVERE, null, ex);
-                
         }
-       
+
     }
 }
